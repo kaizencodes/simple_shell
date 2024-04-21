9d12c4ac9ae8920692c87e55c70ffd48a8eab60f
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2024-04-21 09:52:53 +0100

    implement cd -

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,41 @@
 use std::{
     env,
     io::{self, Write},
+    path::PathBuf,
     process::Command,
 };
 
-pub fn process_input(input: &str) {
+pub fn process_input(input: &str, prev_dir: &mut PathBuf) {
     let args: Vec<&str> = input.trim().split(" ").collect();
     match args[0] {
         "cd" => {
-            cd(args);
+            cd(args, prev_dir);
         }
         "" => {}
         _ => run_command(args),
     };
 }
 
-fn cd(args: Vec<&str>) {
-    let path: &str;
-    let home_env = env::var("HOME").unwrap();
-
+fn cd(args: Vec<&str>, prev_dir: &mut PathBuf) {
+    let curr_dir = env::current_dir().unwrap_or_default();
     if args.len() < 2 {
-        path = &home_env;
+        let home_env = env::var("HOME").unwrap_or(String::from("/"));
+        env::set_current_dir(&home_env).unwrap_or_else(|err| {
+            eprintln!("Couldn't change directory to {home_env} due to {err}")
+        });
+    } else if args[1] == "-" {
+        env::set_current_dir(&prev_dir).unwrap_or_else(|err| {
+            eprintln!(
+                "Couldn't change directory to {} due to {err}",
+                prev_dir.display()
+            )
+        });
     } else {
-        path = args[1];
+        env::set_current_dir(args[1]).unwrap_or_else(|err| {
+            eprintln!("Couldn't change directory to {} due to {err}", args[1])
+        });
     }
-    env::set_current_dir(path).unwrap();
+    *prev_dir = curr_dir;
 }
 
 fn run_command(args: Vec<&str>) {
@@ -34,9 +45,9 @@ fn run_command(args: Vec<&str>) {
     }
 
     match command.output() {
-        Ok(output) => {
-            io::stdout().write_all(&output.stdout).unwrap();
-        }
+        Ok(output) => io::stdout()
+            .write_all(&output.stdout)
+            .unwrap_or_else(|err| eprintln!("Couldn't write to the stdout: {err}")),
         Err(error) => {
             eprintln!("{error}");
         }
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ use simple_shell::process_input;
 fn main() {
     let mut buffer = String::new();
     let stdin = io::stdin();
+    let mut prev_dir = env::current_dir().unwrap_or_default();
     loop {
-        print!("{} > ", env::current_dir().unwrap().display());
-        io::stdout().flush().expect("Could not flush stdout");
+        print!("{} > ", env::current_dir().unwrap_or_default().display());
+        io::stdout()
+            .flush()
+            .unwrap_or_else(|err| eprintln!("Could not flush stdout: {err}"));
         match stdin.read_line(&mut buffer) {
             Ok(_) => {
-                process_input(&buffer);
+                process_input(&buffer, &mut prev_dir);
                 buffer.clear();
             }
             Err(error) => eprintln!("Couldn't read from input: {error}"),
